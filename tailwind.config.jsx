88a4dffa1ab4f649dfcb8925ee25b33e20e45848
sashik mindaka <mindakaofficial0069@gmail.com>
2026-05-27 01:33:45 +0530

    project convert to vite

---
+++ tailwind.config.jsx
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // 🚨 Vite වලට ගැලපෙන නිවැරදිම paths ටික
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
